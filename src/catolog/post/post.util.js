d1a0f1ae7b2f243ff301d3b5ca54c9abf65659c7
duylieu182 <68936232+lieucongduy182@users.noreply.github.com>
2024-08-03 01:30:17 +0700

    handle search api

--- src/catolog/post/post.util.js
+++ src/catolog/post/post.util.js
@@ -2,67 +2,90 @@ import { Sequelize } from 'sequelize';
 import db from '../../../database/models/db';
 const Op = Sequelize.Op;
 
-
-
 export const getPriceCondition = (min, max) => {
-    if (!min && !max) {
-        return {}
-    }
+  if (!min && !max) {
+    return {};
+  }
 
-    if (!max) { // only min
-        return {
-            price: {
-                [Op.gte]: min
-            }
-        }
-    }
-
+  if (!max) {
+    // only min
     return {
-        price: {
-            [Op.between]: [min || 0, max]
-        }
-    }
+      price: {
+        [Op.gte]: min,
+      },
+    };
+  }
 
-}
+  return {
+    price: {
+      [Op.between]: [min || 0, max],
+    },
+  };
+};
 
 export const getCoordinateLatCondition = (min, max) => {
-    if (!min && !max) {
-        return {}
-    }
+  if (!min && !max) {
+    return {};
+  }
 
-    if (!max) { // only min
-        return {
-            lat: {
-                [Op.gte]: min
-            }
-        }
-    }
-
+  if (!max) {
+    // only min
     return {
-        lat: {
-            [Op.between]: [min || 0, max]
-        }
-    }
+      lat: {
+        [Op.gte]: min,
+      },
+    };
+  }
 
-}
+  return {
+    lat: {
+      [Op.between]: [min || 0, max],
+    },
+  };
+};
 
 export const getCoordinateLngCondition = (min, max) => {
-    if (!min && !max) {
-        return {}
-    }
+  if (!min && !max) {
+    return {};
+  }
 
-    if (!max) { // only min
-        return {
-            lng: {
-                [Op.gte]: min
-            }
-        }
-    }
-
+  if (!max) {
+    // only min
     return {
-        lng: {
-            [Op.between]: [min || 0, max]
-        }
-    }
+      lng: {
+        [Op.gte]: min,
+      },
+    };
+  }
 
-}
+  return {
+    lng: {
+      [Op.between]: [min || 0, max],
+    },
+  };
+};
+
+export const getSearchCondition = ({ keywords }) => {
+  if (!keywords) {
+    return {};
+  }
+
+  const whereCondition = {
+    district: {
+      [Op.like]: `%${keywords}%`,
+    },
+    content: {
+      [Op.like]: `%${keywords}%`,
+    },
+    address: {
+      [Op.like]: `%${keywords}%`,
+    },
+    street: {
+      [Op.like]: `%${keywords}%`,
+    },
+  };
+
+  return {
+    [Op.or]: whereCondition,
+  };
+};
